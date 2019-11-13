b448ab606f11a53345ea121c80bd0feae0b327fb
ktm12 <ktm12@LAPTOP-QQFVN6GJ>
2019-11-13 13:00:09 +0900

    이름을 입력받아서 간단한 편지를 써 주는 프로그램.

---
+++ GitHubTest/src/RandomLetter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RandomLetter {
+
+	public static void main(String[] args) {			// 이름을 입력받아서 간단한 편지를 써 주는 프로그램
+		// TODO Auto-generated method stub
+		String [] firstWord = {"안녕하세요", "어서 오세요", "반갑습니다"};
+		String [] secondWord = {"방문해주셔서 감사합니다", "또 오셨네요"};
+		String [] thirdWord = {"다시 한 번 감사드립니다", "잘 해 드릴게요", "네!"};
+		
+		int firstNum = (int)(Math.random()*firstWord.length);
+		int secondNum = (int)(Math.random()*secondWord.length);
+		int thirdNum = (int)(Math.random()*thirdWord.length);
+		
+		
+		System.out.println("이름을 입력해주세요: ");
+		Scanner kb = new Scanner(System.in);
+		
+		String name = kb.next(); 
+		
+		System.out.println(name+"님 "+firstWord[firstNum]+ " "+secondWord[secondNum]+" "+thirdWord[thirdNum]);
+		
+		
+	}
+
+}
