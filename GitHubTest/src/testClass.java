c529f005b1822f3ec94c63b0f4088c04445f0299
ktm12 <ktm12@LAPTOP-QQFVN6GJ>
2019-11-13 14:10:43 +0900

    okay~! 계획대로 되고 있어

--- GitHubTest/src/testClass.java
+++ GitHubTest/src/testClass.java
@@ -4,6 +4,7 @@ public class testClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is for test");
+		System.out.println("아까는 왜 그딴 오류가 있었던걸까");
 		
 	}
 
