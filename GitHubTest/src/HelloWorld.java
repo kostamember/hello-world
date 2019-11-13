51c4192a52fc1a1701f696226b4b0cbfa8761970
kostamember <ktm1296@naver.com>
2019-11-13 12:35:43 +0900

    Update HelloWorld.java

--- GitHubTest/src/HelloWorld.java
+++ GitHubTest/src/HelloWorld.java
@@ -3,7 +3,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		int n=10;
+		for(int i=0; i<n; i++) {
+			System.out.println("Hello world");
+		}
 	}
 
 }
